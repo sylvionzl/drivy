d17dba111bcb574ea3ca66e0cdc22c0c2f360bb7
Sylvain Giachino <sylvain.kino@gmail.com>
2017-12-07 15:58:48 +0100

    drivy

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -11,7 +16,7 @@ app.get('/form', function (req, res) {
   res.render('form');
 });
 
-app.get('/formulaire', function (req, res) {
+app.post('/formulaire', upload.array() function (req, res) {
   console.log(req.query);
   res.send('OK');
 });
@@ -21,3 +26,5 @@ var port = (process.env.PORT || 8080);
 app.listen(port, function () {
   console.log("Server listening on port 8080");
 });
+
+//il faut aussi utiliser Multer (à installer sur le terminal : npm install --save multer)
