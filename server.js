335c6def05c02336afb302dfe7ac0eb5ed6b9490
Sylvain Giachino <sylvain.kino@gmail.com>
2017-12-07 17:34:21 +0100

    drivy

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ var app = express();
 var bodyParser = require('body-parser');
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
+var request = require('request');
+var mongoose= require('mongoose');
+var options = { server: { socketOptions: {connectTimeoutMS: 30000 } }};
+mongoose.connect('mongodb://drivyapp:azerty@ds131546.mlab.com:31546/drivybdd', options, function(err) {
+  console.log(err);
+});
+var userSchema = mongoose.Schema({
+    modele: String,
+    marque: String,
+    ville: String,
+    places: String,
+    lng: Number,
+    lat: Number
+});
+var userModel = mongoose.model('User', userSchema);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -18,6 +33,29 @@ app.get('/form', function (req, res) {
 
 app.post('/formulaire', upload.array(), function (req, res) {
   console.log(req.body);
+
+  request("https://maps.googleapis.com/maps/api/geocode/json?address="+req.body.ville+"&key=AIzaSyDgwAecvLC9cbyAdtBZQAfFmfP3dH4EQYk", function(error, response, data) {
+
+    var data = JSON.parse(data);
+    var lat = data.results[0].geometry.location.lat;
+    var lon = data.results[0].geometry.location.lng;
+    console.log(lat);
+    console.log(lon);
+
+  var user = new userModel ({
+    name: req.body.modele,
+    marque: req.body.marque,
+    ville: req.body.ville,
+    places: req.body.places,
+    lng: lon,
+    lat: lat
+    });
+
+    user.save(function (error, contact) {});
+
+      });
+
+
   res.send('OK');
 });
 
